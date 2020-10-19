dfadc34e86e2dded81636b875426a0d4fc9b93af
Anirudh <sanjeev196945@gmail.com>
2020-10-19 18:17:24 +0530

    :lipstick: Major UI Changes
    Added Validation and changed theme overall

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import SignupScreen from './src/screens/SignupScreen';
+import SignupScreen from './src/screens/SignupScreen';  
 import FirstScreen from './src/screens/FirstScreen';
 import AdminDash  from './src/screens/AdminDash'
 import ShopScreen from './src/screens/ShopScreen'
